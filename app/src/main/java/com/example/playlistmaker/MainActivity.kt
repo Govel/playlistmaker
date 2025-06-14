c932844965e8b513791a948af412aa0704fd12f2
Govel <s.gorlov123@gmail.com>
2025-06-14 17:35:15 +0300

    Added click listeners for button

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val buttonSearch = findViewById<Button>(R.id.search_button)
+        buttonSearch.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Кнопка поиска", Toast.LENGTH_SHORT).show()
+        }
+        val buttonSettings = findViewById<Button>(R.id.settings_button)
+        val buttonSettingsClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Кнопка настроек", Toast.LENGTH_SHORT).show()
+            }
+        }
+        buttonSettings.setOnClickListener(buttonSettingsClickListener)
+        val buttonMedia = findViewById<Button>(R.id.media_button)
+        buttonMedia.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Кнопка медиа", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
