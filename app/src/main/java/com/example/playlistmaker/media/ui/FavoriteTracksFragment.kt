6a5a1eb267c34f561d3f443bc673ece6550fbb76
Govel <s.gorlov123@gmail.com>
2025-11-27 16:07:37 +0300

    Added Media Library with Fragments + MVVM pattern

---
+++ app/src/main/java/com/example/playlistmaker/media/ui/FavoriteTracksFragment.kt
@@ -0,0 +1,32 @@
+package com.example.playlistmaker.media.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.playlistmaker.databinding.FragmentFavoriteTracksBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class FavoriteTracksFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteTracksBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel by viewModel<FavoriteTracksViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteTracksBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+
+    companion object {
+        fun newInstance() = FavoriteTracksFragment()
+    }
+}
